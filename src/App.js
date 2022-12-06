8b4e286512a2ca54d87abbf309309ffea5070ea1
inga-divra <ingady@gmail.com>
2022-12-06 11:44:44 +0200

    project start & header style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <header className="header">
+
+          <div className="header__left">
+            <a className="logo header__logo" href="#" >
+              <img className="logo__img" src="./img/logo.svg" alt="React Sneakers logo" width={40} height={40} />
+            </a>
+            <div className="header__info">
+              <p className="header__title">React Sneakers</p>
+              <p className="header__subtitle">The best sneakers on the market</p>
+            </div>
+          </div>
+          <ul className="header__right">
+            <li className="header__right-item">
+              <img className="header__icon" src="./img/card.svg" alt="shopping card icon" width={20} height={20} />
+              <span className="header__price">199 €</span>
+            </li>
+            <li className="header__right-item">
+              <img className="header__icon" src="./img/user.svg" alt="user icon" width={20} height={20} />
+            </li>
+          </ul>
+
+        </header>
+      </div>
+
+      <main>
+        <section className="main">
+          <div className="container">
+            <h1 className="main__title">All sneakers</h1>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
