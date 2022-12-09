483f101fcd30a8935c8f9055e2f0081570ef6276
inga-divra <ingady@gmail.com>
2022-12-09 10:38:52 +0200

    fetch products on the  main page

--- src/App.js
+++ src/App.js
@@ -2,80 +2,37 @@ import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Product from "./components/Product/Product.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+const url = 'https://6392d87311ed187986a2605e.mockapi.io/products';
 
-const products = [
-  {
-    name: "Men's sneakers Nike Blazer Mid Suede",
-    price: 110.00,
-    imgUrl: "./img/sneakers/1.jpg"
-  },
-  {
-    name: "Men's sneakers Nike Air Max 270",
-    price: 160.00,
-    imgUrl: "./img/sneakers/2.jpg"
-  },
-  {
-    name: "Men's sneakers Nike Blazer Mid White",
-    price: 109.99,
-    imgUrl: "./img/sneakers/3.jpg"
-  },
-  {
-    name: "Women's sneakers Puma X Future Rider",
-    price: 64.95,
-    imgUrl: "./img/sneakers/4.jpg"
-  },
-  {
-    name: "Unisex Under Armour Curry 8",
-    price: 128.00,
-    imgUrl: "./img/sneakers/5.jpg"
-  },
-  {
-    name: "Men's sneakers Nike Kyrie 7",
-    price: 99.95,
-    imgUrl: "./img/sneakers/6.jpg"
-  },
-  {
-    name: "Men's sneakers Jordan Air Jordan 11",
-    price: 125.00,
-    imgUrl: "./img/sneakers/7.jpg"
-  },
-  {
-    name: "Men's sneakers Nike LeBron XVIII",
-    price: 139.00,
-    imgUrl: "./img/sneakers/8.jpg"
-  },
-  {
-    name: "Women's sneakers Nike Lebron XVIII Low",
-    price: 115.00,
-    imgUrl: "./img/sneakers/9.jpg"
-  },
-  {
-    name: "Men's sneakers Nike Blazer Mid Green",
-    price: 119.00,
-    imgUrl: "./img/sneakers/1.jpg"
-  },
-  {
-    name: "Women's sneakers Puma X Future Rider",
-    price: 60.95,
-    imgUrl: "./img/sneakers/4.jpg"
-  },
-  {
-    name: "Unisex Nike Kyrie Flytrap IV",
-    price: 129.00,
-    imgUrl: "./img/sneakers/10.jpg"
-  }
-]
 
 
 
 function App() {
+  const [products, setProducts] = useState([]);
   const [cartOpened, setCartOpened] = useState(false);
+  const [cartItems, setCartItems] = useState([1, 2]);
+
+
+  const fetchProducts = async () => {
+    try {
+      const resp = await fetch(url)
+      const data = await resp.json()
+      return setProducts(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
 
   return (
     <div className="App">
       <div className="wrapper">
-        {cartOpened && <Drawer onClose={() => setCartOpened(false)} />}
+        {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
         <Header onClickCart={() => setCartOpened(true)} />
         <main>
 
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,35 +1,28 @@
-function Drawer(props) {
+function Drawer({ onClose, items = [] }) {
     return (
         <div className="overlay">
             <div className="drawer">
                 <div className="drawer__top">
                     <h2 className="title drawer__title underline drawer__underline">
                         My cart</h2>
-                    <button className="drawer__close-btn" onClick={props.onClose}>
+                    <button className="drawer__close-btn" onClick={onClose}>
                         <img className="drawer__close-btn_icon" src="./img/icons/delete_btn.svg" alt="close cart icon" width={32} height={32} />
                     </button>
                 </div>
                 <div className="drawer__cards">
-                    <div className="drawer__card">
-                        <img className="drawer__card-img" src="./img/sneakers/2.jpg" alt="Nike Air Max 270" width={70} height={70} />
-                        <div className="drawer__card-desc">
-                            <h3 className="drawer__card-title">Men's sneakers <span className="drawer__card-subtitle">Nike Air Max 270</span></h3>
-                            <span className="price">109,99€</span>
+                    {items.map((obj) => {
+                        <div className="drawer__card">
+                            <img className="drawer__card-img" src="./img/sneakers/2.jpg" alt="Nike Air Max 270" width={70} height={70} />
+                            <div className="drawer__card-desc">
+                                <h3 className="drawer__card-title">Men's sneakers <span className="drawer__card-subtitle">Nike Air Max 270</span></h3>
+                                <span className="price">109,99€</span>
+                            </div>
+                            <button className="drawer__card-btn" type="button">
+                                <img className="drawer__card-btn-icon" src="./img/icons/delete_btn.svg" alt="delete product from the cart icon" width={32} height={32} />
+                            </button>
                         </div>
-                        <button className="drawer__card-btn" type="button">
-                            <img className="drawer__card-btn-icon" src="./img/icons/delete_btn.svg" alt="delete product from the cart icon" width={32} height={32} />
-                        </button>
-                    </div>
-                    <div className="drawer__card">
-                        <img className="drawer__card-img" src="./img/sneakers/4.jpg" alt="Nike Air Max 270" width={70} height={70} />
-                        <div className="drawer__card-desc">
-                            <h3 className="drawer__card-title">Women's sneakers <span className="drawer__card-subtitle">Puma X Future Rider</span></h3>
-                            <span className="price">64,95€</span>
-                        </div>
-                        <button className="drawer__card-btn" type="button">
-                            <img className="drawer__card-btn-icon" src="./img/icons/delete_btn.svg" alt="delete product from the cart icon" width={32} height={32} />
-                        </button>
-                    </div>
+
+                    })}
                 </div>
                 <div className="drawer__bottom">
                     <ul className="drawer__bottom-items">
